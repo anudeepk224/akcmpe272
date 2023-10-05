cd97f36968a933c7babe0b1b3a079a50ec02aeb0
anudeepk224 <48233893+anudeepk224@users.noreply.github.com>
2023-10-04 17:42:33 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
-  echo "Hello, World!";
+  // Array of inspirational quotes
+  $quotes = [
+    "Believe you can and you're halfway there. -Theodore Roosevelt",
+    "The only way to do great work is to love what you do. -Steve Jobs",
+    "Your time is limited, don't waste it living someone else's life. -Steve Jobs",
+    "It always seems impossible until it's done. -Nelson Mandela",
+    "The future belongs to those who believe in the beauty of their dreams. -Eleanor Roosevelt"
+  ];
+
+  // Get a random quote from the array
+  $randomQuote = $quotes[array_rand($quotes)];
+
+  // Output the quote
+  echo "<blockquote><p>$randomQuote</p></blockquote>";
 ?>
